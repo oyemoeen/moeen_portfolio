b8779e09697ec316aa5c91871c87fec7e7b04c9a
moeenali <moeenali0124@gmail.com>
2025-06-27 11:52:18 +0530

    add and delete uncessary image

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Hero from "../src/components/Hero"
-import Navbar from "../src/components/Navbar"
-import About from '../src/components/About'
-import Skills from '../src/components/Skills'
-import ProjectsPage from '../src/components/Project'
-import Contact from '../src/components/Contact'
+import { useState } from "react";
+import "./App.css";
+import Hero from "../src/components/Hero";
+import Navbar from "../src/components/Navbar";
+import About from "../src/components/About";
+import Skills from "../src/components/Skills";
+import ProjectsPage from "../src/components/Project";
+import Contact from "../src/components/Contact";
+import viteLogo from "../public/vite.svg";
+import reactLogo from "./assets/react.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Hero/>
+      <Hero />
       {/* <Navbar/> */}
-      <About/>
-      <Skills/>
-      <ProjectsPage/>
-      <Contact/>
+      <About />
+      <Skills />
+      <ProjectsPage />
+      <Contact />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
